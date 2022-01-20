f8a125bf5cd93c1febd1d1819cf2ff81bfd6685f
ddelah98 <ddelahoussaye@alliancesafetycouncil.org>
2022-01-20 09:46:59 -0600

    Update - Remove GetNotes

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,10 +1,6 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
-const getNotes = () => {
-    return 'Your notes...'
-}
-
 const addNote =(title, body) => {
     const notes = loadNotes()
     //const duplicateNotes = notes.filter((note)=> note.title === title)
@@ -72,7 +68,6 @@ const readNotes = (title) =>{
 }
 
 module.exports = {
-    getNotes : getNotes,
     addNote : addNote,
     removeNote : removeNote,
     listNotes : listNotes,
