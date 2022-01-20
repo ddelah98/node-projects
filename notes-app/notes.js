b789c49d290a47b368ab8de2a23581f5b5e3906a
ddelah98 <ddelahoussaye@alliancesafetycouncil.org>
2022-01-20 07:59:44 -0600

    Update - Remove Note function creation

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -39,7 +39,12 @@ const loadNotes = function(){
     }
 }
 
+const removeNote = function(title){
+    console.log('removing title' + title)
+}
+
 module.exports = {
     getNotes : getNotes,
-    addNote : addNote
+    addNote : addNote,
+    removeNote : removeNote
 }
