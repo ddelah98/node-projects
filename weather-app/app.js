1a0e19544f4a62ee1f258b8415d5b667458b6c14
ddelah98 <ddelahoussaye@alliancesafetycouncil.org>
2022-01-23 18:32:49 -0600

    UPDATE - Read Address from command line

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,16 +1,22 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-
-geocode('Boston', (error, data) => {
-    if(error){
-        return console.log(error);
-    }
-    forecast(data.latitude, data.longitude, (error, forecastData) =>{
+const address = process.argv[2]
+if(!address)
+{
+    console.log('Please enter an address');
+}else{
+    geocode(address, (error, data) => {
         if(error){
             return console.log(error);
         }
-        console.log(data.location);
-        console.log(forecastData)
+        forecast(data.latitude, data.longitude, (error, forecastData) =>{
+            if(error){
+                return console.log(error);
+            }
+            console.log(data.location);
+            console.log(forecastData)
+        })
     })
-})
+    
+}
