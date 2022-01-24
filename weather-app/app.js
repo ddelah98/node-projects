a41a5b162649520549f83e482497edf74cb0debe
ddelah98 <ddelahoussaye@alliancesafetycouncil.org>
2022-01-23 19:14:01 -0600

    UPDATE - Use Deconstructuring on objects

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,17 +6,16 @@ if(!address)
 {
     console.log('Please enter an address');
 }else{
-    geocode(address, (error, data) => {
+    geocode(address, (error, { latitude, longitude, location} = {}) => {
         if(error){
             return console.log(error);
         }
-        forecast(data.latitude, data.longitude, (error, forecastData) =>{
+        forecast(latitude, longitude, (error, forecastData) =>{
             if(error){
                 return console.log(error);
             }
-            console.log(data.location);
+            console.log(location);
             console.log(forecastData)
         })
     })
-    
 }
